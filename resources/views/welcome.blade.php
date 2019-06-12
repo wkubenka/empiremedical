48a86cdecf7ccea1ebcffde1eab9cc6910f73407
Will Kubenka <wkubenka@gmail.com>
2019-06-12 13:51:13 -0400

    Added ability to sign in and play against the computer.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,64 +4,106 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-
-        <!-- Styles -->
+        <title>Tic-Tac-Toe</title>
+        <!-- TODO: Move this style to its own file -->
         <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
+            body {
+            font: 14px "Century Gothic", Futura, sans-serif;
+            margin: 20px;
             }
 
-            .full-height {
-                height: 100vh;
+            ol, ul {
+            padding-left: 30px;
             }
 
-            .flex-center {
-                align-items: center;
+            .board-row:after {
+            clear: both;
+            content: "";
+            display: table;
+            }
+
+            .status {
+            margin-bottom: 10px;
+            }
+
+            .square {
+            background: #fff;
+            border: 1px solid #999;
+            float: left;
+            font-size: 24px;
+            font-weight: bold;
+            line-height: 34px;
+            height: 34px;
+            margin-right: -1px;
+            margin-top: -1px;
+            padding: 0;
+            text-align: center;
+            width: 34px;
+            }
+
+            .square:focus {
+            outline: none;
+            }
+
+            .kbd-navigation .square:focus {
+            background: #ddd;
+            }
+
+            .game {
                 display: flex;
-                justify-content: center;
+                flex-direction: row;
+                margin: auto;
             }
 
-            .position-ref {
+            .game-info {
+            margin-left: 20px;
+            }
+            .scoreboard {
+                display: table;
+                margin: auto;
+            }
+
+            .score,
+            .team,
+            .letter {
+                display: table-row;
+            }
+
+            .score span,
+            .team span,
+            .letter span {
+                display: table-cell;
+            }
+
+            .score span:first-child,
+            .team span:first-child,
+            .letter span:first-child {
+                text-align: right;
+                padding-right: 10px;
                 position: relative;
             }
 
-            .top-right {
+            .score span:last-child,
+            .team span:last-child,
+            .letter span:last-child {
+                text-align: left;
+                padding-left: 10px;
+            }
+
+            /*the line between scores*/
+            .score span:first-child::before,
+            .team span:first-child::before,
+            .letter span:first-child::before {
+                content: "-";
                 position: absolute;
-                right: 10px;
-                top: 18px;
+                right:-2px;
+                top: 0;
             }
 
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
         </style>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
     </head>
     <body>
         <div class="flex-center position-ref full-height">
@@ -79,21 +121,118 @@
                 </div>
             @endif
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+            <div id="app">
+                <div class="scoreboard">
+                    <div class="score">
+                        <span>@{{ player1Wins }}</span><span>@{{ player2Wins }}</span>
+                    </div>
+                    <div class="team">
+                        <span>Player 1</span><span>@{{ player2Name }}</span>
+                    </div>
+                    <div class="team">
+                        <span>X</span><span>O</span>
+                    </div>
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="game">
+                    <div class="game-board">
+                        <div>
+                            <div class="status">Next player: @{{nextPlayer}}</div>
+                            <div class="board-row">
+                                <button class="square" @click="clickSquare(0)">@{{ boardData[0] }}</button>
+                                <button class="square" @click="clickSquare(1)">@{{ boardData[1] }}</button>
+                                <button class="square" @click="clickSquare(2)">@{{ boardData[2] }}</button>
+                            </div>
+                            <div class="board-row">
+                                <button class="square" @click="clickSquare(3)">@{{ boardData[3] }}</button>
+                                <button class="square" @click="clickSquare(4)">@{{ boardData[4] }}</button>
+                                <button class="square" @click="clickSquare(5)">@{{ boardData[5] }}</button>
+                            </div><div class="board-row">
+                                <button class="square" @click="clickSquare(6)">@{{ boardData[6] }}</button>
+                                <button class="square" @click="clickSquare(7)">@{{ boardData[7] }}</button>
+                                <button class="square" @click="clickSquare(8)">@{{ boardData[8] }}</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="game-info">
+                        <div>
+                            <p v-if="winner !== null">
+                                @{{ winner  }} won the game!
+                            </p>
+                            <a @click="reset">Reset Game</a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
+        <script src="https://cdn.jsdelivr.net/npm/vue/dist/vue.js"></script>
+        <script>
+            //TODO: Move this to its own file
+            let app = new Vue({
+                el: '#app',
+                data: {
+                    player1Wins: 0,
+                    player2Wins: 0,
+                    player1IsNext: true,
+                    player2IsComputer: true,
+                    boardData: Array(9).fill(null),
+                    winner: null,
+                },
+                computed: {
+                    nextPlayer: function() {
+                        return this.player1IsNext  ? 'X' : 'O';
+                    },
+                    player2Name: function() {
+                        return this.player2IsComputer ? "Computer" : "Player 2";
+                    }
+                },
+                methods: {
+                    calculateWinner: function() {
+                        const lines = [
+                            [0, 1, 2],
+                            [3, 4, 5],
+                            [6, 7, 8],
+                            [0, 3, 6],
+                            [1, 4, 7],
+                            [2, 5, 8],
+                            [0, 4, 8],
+                            [2, 4, 6],
+                        ];
+                        for (let i = 0; i < lines.length; i++) {
+                            const [a, b, c] = lines[i];
+                            if (this.boardData[a] && this.boardData[a] === this.boardData[b] && this.boardData[a] === this.boardData[c]) {
+                                this.winner = this.boardData[a];
+                                if(this.boardData[a] === "X") this.player1Wins++;
+                                else this.player2Wins++;
+                            }
+                        }
+                        if(this.winner === null && !this.boardData.includes(null)){
+                             this.winner = 'Cat';
+                        }
+                    },
+                    clickSquare: function(i) {
+                        if(this.boardData[i] !== null || this.winner !== null) return;
+                        this.$set(this.boardData, i, this.player1IsNext  ? 'X' : 'O');
+                        this.player1IsNext = !this.player1IsNext;
+                        this.calculateWinner();
+                        if(!this.player1IsNext && this.player2IsComputer) this.computerMove();
+                    },
+                    computerMove: function() {
+                        if(this.winner !== null) return;
+                        let i = Math.floor(Math.random() * 9);
+                        while(this.boardData[i] !== null) {
+                            i = Math.floor(Math.random() * 9);
+                        }
+                        this.clickSquare(i);
+                    },
+                    reset: function() {
+                        this.player1IsNext = Math.random() > .5;
+                        this.boardData = Array(9).fill(null);
+                        this.winner = null;
+                        if(!this.player1IsNext && this.player2IsComputer) this.computerMove();
+                    }
+                }
+            })
+        </script>
     </body>
 </html>
